8d314a75ae39657b569a694bdbe05ad6a7592602
Noahxyzs <105595182+Noahxyzs@users.noreply.github.com>
2023-12-19 21:10:09 +0300

    Add files via upload

---
+++ alx-disney-plus-clone/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import "./App.css";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
